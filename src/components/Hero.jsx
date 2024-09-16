ee5947f8af99d376a24c41379432a66c866062fa
yongqi8899 <1243994415@qq.com>
2024-09-16 10:58:23 +0200

    change CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
         })}
         <div className="mt-10 ">
           <Link
-            to="https://drive.google.com/file/d/1TC-5JcqCLbU-qae6QqC5Q1NIi41MOQtP/view?usp=drive_link"
+            to="https://drive.google.com/file/d/18n9WDs8lPC0IHSbd7g6JqSDNN9t6YE2_/view?usp=drive_link"
             target="_blank"
             className="px-6 py-2 text-lg font-bold border border-violet-500 violet-gradient rounded-xl hover:bg-blue-400"
           >
