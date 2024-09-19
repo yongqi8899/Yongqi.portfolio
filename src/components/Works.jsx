8a56b923a92ae4cd9ba19c7c78ac2308afdb7fef
yongqi8899 <1243994415@qq.com>
2024-09-19 09:22:50 +0200

    change works style

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,62 +17,57 @@ const ProjectCard = ({
   deploy_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full  group"
-      >
-        <div className="relative w-full h-[230px]">
-          <img src={image} alt="project_image" className="w-full" />
-          <div className="absolute inset-0 flex justify-end gap-2 m-3 opacity-0 card-img_hover group-hover:opacity-100">
-            {source_code_link && (
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer black-gradient"
-              >
-                <img
-                  src={github}
-                  alt="source code"
-                  className="object-contain w-1/2 h-1/2"
-                />
-              </div>
-            )}
-            {deploy_link && (
-              <div
-                onClick={() => window.open(deploy_link, "_blank")}
-                className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer black-gradient"
-              >
-                <img
-                  src={internet}
-                  alt="source code"
-                  className="object-contain w-1/2 h-1/2"
-                />
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-
-        <div className="flex flex-wrap gap-2 mt-4">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full  group"
+    >
+      <div className="relative w-full h-[230px]">
+        <img src={image} alt="project_image" className="w-full" />
+        <div className="absolute inset-0 flex justify-end gap-2 m-3 opacity-0 card-img_hover group-hover:opacity-100">
+          {source_code_link && (
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer black-gradient"
             >
-              #{tag.name}
-            </p>
-          ))}
+              <img
+                src={github}
+                alt="source code"
+                className="object-contain w-1/2 h-1/2"
+              />
+            </div>
+          )}
+          {deploy_link && (
+            <div
+              onClick={() => window.open(deploy_link, "_blank")}
+              className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer black-gradient"
+            >
+              <img
+                src={internet}
+                alt="source code"
+                className="object-contain w-1/2 h-1/2"
+              />
+            </div>
+          )}
         </div>
-      </Tilt>
-    </motion.div>
+      </div>
+
+      <div className="mt-5">
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
+
+      <div className="flex flex-wrap gap-2 mt-4">
+        {tags.map((tag) => (
+          <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </Tilt>
   );
 };
 
