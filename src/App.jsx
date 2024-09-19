d8876b1dc9a72a13b76c8577e858683f627cd876
yongqi8899 <1243994415@qq.com>
2024-09-19 08:25:51 +0200

    add projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
 
 const Contact = lazy(() => import("./components/Contact"));
+const Experience = lazy(() => import("./components/Experience"));
 const Hero = lazy(() => import("./components/Hero"));
 const Navbar = lazy(() => import("./components/Navbar"));
 const Tech = lazy(() => import("./components/Tech"));
+const Works = lazy(() => import("./components/Works"));
 
 const App = () => {
   return (
@@ -16,6 +19,8 @@ const App = () => {
             <Hero />
           </div>
           <Tech />
+          <Works />
+          <Experience />
           <div className="relative z-0">
             <Contact />
           </div>
